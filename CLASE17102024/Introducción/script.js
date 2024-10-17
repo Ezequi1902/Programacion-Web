616f2c8be33ec5040596b8fbbfb8b1efc15e813b
Ezequi1902 <ddominguezd2201@alumno.ipn.mx>
2024-10-17 16:40:32 -0600

    ezepapu

--- CLASE17102024/Introducción/script.js
+++ CLASE17102024/Introducción/script.js
@@ -4,7 +4,7 @@ document.title = 'Iniciar Sesión';
 const container = document.createElement('div');
 container.style.margin = '20px';
 //crear titulo
-const tilte = document.createElement('h2');
+const title = document.createElement('h2');
 title.textContent = 'Iniciar Sesión';
 container.appendChild(title);
 
@@ -23,12 +23,50 @@ usernameInput.required = true;
 form.appendChild(usernamelabel);
 form.appendChild(usernameInput);
 
-//Contraseña
-const usernamelabel = document.createElement('label');
-usernamelabel.textContent = 'Contraseña';
 
-const usernameInput = document.createElement('input');
-usernameInput.type = 'text';
-usernameInput.required = true;
-form.appendChild(usernamelabel);
-form.appendChild(usernameInput);
+//Contraseña
+const passwordlabel = document.createElement('label');
+passwordlabel.textContent = 'Contraseña';
+
+const paswordInput = document.createElement('input');
+passwordlabel.type = 'password';
+paswordInput.required = true;
+
+form.appendChild(passwordlabel);
+form.appendChild(paswordInput);
+
+
+//Boton de envio
+const submitButton = document.createElement('button');
+submitButton.type= 'submit';
+submitButton.textContent= 'Iniciar sesión';
+form.appendChild(submitButton);
+
+//Mensaje de respuesta
+const  message = document.createElement('p');
+container. appendChild(form);
+container.appendChild(message);
+
+//Añadir el contenedor al cuerpo del documento (se muestran los datos jijij)
+document.body.appendChild(container);
+
+//DATOS DE INICIO DE SESION PREDEFINIDA
+
+const validaUSER = 'juanito';
+const validaPassword = '2589';
+ 
+
+//validar el user y el pass
+form.addEventListener('submit',(event) =>{
+    event.preventDefault.apply();
+
+    //validar el nombre y el pass
+
+    if (usernameInput.value === validaUSER && paswordInput.value === validaPassword ) {
+        message.textContent = 'Bienvenido!';
+        message.style.color = 'green';
+    }else{
+        message.textContent = 'Nombre de usuario o contraseña incorrecto'
+        message.style.color = 'red';
+    }
+})
