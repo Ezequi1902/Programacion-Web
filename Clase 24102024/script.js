b1f3c6eed4d90d6b3a85c5ebfb4da863179bb9a2
Ezequi1902 <ddominguezd2201@alumno.ipn.mx>
2024-10-24 16:19:10 -0600

    odaa

---
+++ Clase 24102024/script.js
@@ -0,0 +1,18 @@
+/*
+//Vamos a ver arreglos
+let array = ["dato1",1,Boolean];
+console.log("Tamaño del array: "+ array.length);
+array.push("2");
+console.log(array[3]);
+console.log("Tamaño del nuevo array: "+ array.length);
+//Tabla del 5
+*/
+let x = [];
+for (var i = 0; i <=10; i++) {
+    y = i*5;
+    x.push("5 x "+ i + "= " + y);
+    if (i<=6) {
+        console.log(x[i]);
+    }
+}
+console.log("Trerminado en la posición: "+x.length)
