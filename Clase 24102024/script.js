664526b79bb583a01395dde83de80456fb41ea14
Ezequi1902 <ddominguezd2201@alumno.ipn.mx>
2024-10-24 16:35:50 -0600

    odaa

--- Clase 24102024/script.js
+++ Clase 24102024/script.js
@@ -6,7 +6,7 @@ array.push("2");
 console.log(array[3]);
 console.log("Tamaño del nuevo array: "+ array.length);
 //Tabla del 5
-*/
+
 let x = [];
 for (var i = 0; i <=10; i++) {
     y = i*5;
@@ -16,3 +16,12 @@ for (var i = 0; i <=10; i++) {
     }
 }
 console.log("Trerminado en la posición: "+x.length)
+
+let array = ["dato1",1,Boolean];
+array.push("insert nuevo val");
+array.push("gato");
+console.log(array[3]);
+//Eliminar
+array.splice(2,1);
+console.log(array);
+*/
